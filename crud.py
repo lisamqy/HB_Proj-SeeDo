0b28de3a09114a96e536e75b6f9e43200fa53e8d
Lisa Ma <lisamqy@gmail.com>
2021-08-18 08:27:28 +0000

    Added in some more functions

--- crud.py
+++ crud.py
@@ -14,10 +14,10 @@ def create_user(email, password):
     return user
 
 
-def create_location(zipcode, cityname, countryname):
+def create_location(zipcode, cityname, countryname,plan_id=None):
     """Create and return a new location."""
 
-    location = Location(zipcode=zipcode, cityname=cityname, countryname=countryname)
+    location = Location(zipcode=zipcode, cityname=cityname, countryname=countryname, plan_id=plan_id)
 
     db.session.add(location)
     db.session.commit()
@@ -25,8 +25,48 @@ def create_location(zipcode, cityname, countryname):
     return location
 
 
-def create_event():
-    pass
+def create_plan(user_id=None):
+    """Create and return a new plan."""
+
+    plan = Plan(user_id=user_id)
+
+    db.session.add(plan)
+    db.session.commit()
+
+    return plan  
+
+
+def create_image(image_location=None, plan_id=None):
+    """Create and return a new image."""
+
+    image = Image(image_location=image_location, plan_id=plan_id)
+
+    db.session.add(image)
+    db.session.commit()
+
+    return plan      
+
+
+def create_event(location_id, overview=None, datetime=None, plan_id=None):
+    """Create and return a new event."""
+
+    event = Event(location_id=location_id, overview=overview, datetime=datetime, plan_id=plan_id)
+
+    db.session.add(event)
+    db.session.commit()
+
+    return event
+
+
+def create_theme(tag=None, overview=None):
+    """Create and return a new theme."""
+
+    theme = Theme(tag=tag, overview=overview)
+
+    db.session.add(theme)
+    db.session.commit()
+
+    return theme     
 
 
 if __name__ == '__main__':
