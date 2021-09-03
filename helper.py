6d0dac1f3345bcf3696b559ff5f12a1a65760ead
Lisa Ma <lisamqy@gmail.com>
2021-09-03 01:57:51 +0000

    Updated features and added bug fixes

--- helper.py
+++ helper.py
@@ -1,12 +1,3 @@
-# all_events = {
-#   'Van Gogh': {
-#       'venue': 'blah blah',
-#       'events': [
-#                   {'id': 12345, 'start_time': '2021-11-01 12:00', 'end_time': '2021-11-01 12:30'},
-#                   {'id': 56789, 'start_time': '2021-11-01 12:30', 'end_time': '2021-11-01 1:00'}
-#               ]
-#         }
-# }
 
 def clean_search_results(tm_response):
     #parse through the event search results 
@@ -16,7 +7,7 @@ def clean_search_results(tm_response):
     for tm_event in tm_response: 
         # set a name key
         name = tm_event['name']
-        image = tm_event['images'][0]
+        image = tm_event['images'][0]['url']
         venue = tm_event['_embedded']['venues'][0]['name'] 
         start_date = tm_event['dates']['start']['localDate']
         # start_time = tm_event['dates']['start']['localTime'] #NOTE shows have multiple show times so might cause user confusion if i add
