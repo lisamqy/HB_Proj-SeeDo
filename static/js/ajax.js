32655c22a7fd0d88559aea490c7f0abe26612545
Lisa Ma <lisamqy@gmail.com>
2021-09-10 18:49:38 +0000

    Polish features + bug fixes

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -5,28 +5,28 @@ $('#event-adder').on('click', () => {
     if($('#event-item').val().length>0) {
         $('#event-list').append(`<li>${$('#event-item').val()}</li>`)
     }
-})
+});
 
 
 //gets the like button to communicate with server.py's function
 $('.like-button').on('click', (evt) => {
     const formInputs = { eventId : evt.target.value }; //gets the button's value aka -> eventId : event.event_id
-    $.post("/handle-likes", formInputs, (res) => {
+    $.post('/handle-likes', formInputs, (res) => {
         console.log(res) //prints out whatever the route returns on server.py 
-        $('.liked-response').html("Thanks for the like!")
+        $('.liked-response').html('Thanks for the like!')
         let currentLike = parseInt($('#like-counter').html())
         $('#like-counter').html(currentLike+1)
-        $('.like-button').attr("disabled", "disabled"); // disable button
+        $('.like-button').attr('disabled', 'disabled'); // disable button
     })
-})
+});
 
 
 //allow user to hide/show event overview details editor
-$("input[name=btnEditOverview]").on('click', () => {
-    if ($("#dvEditOverview").hasClass("hide")) {
-        $("#dvEditOverview").removeClass("hide");
+$('input[name=btnEditOverview]').on('click', () => {
+    if ($('#dvEditOverview').hasClass('hide')) {
+        $('#dvEditOverview').removeClass('hide');
     } else {
-        $("#dvEditOverview").addClass("hide")
+        $('#dvEditOverview').addClass('hide');
     }
      
 });
@@ -40,10 +40,12 @@ $('.del-event-from-plan-btn').on('click', (evt) => {
         console.log(res) //prints out whatever the route returns on server.py 
         $(`#li${event_id}`).remove();
     })
-})
+});
 
-//guides user to create/search for events if none found for current city
-if ($('#dropdown-event')[0].length < 1 ) {
-    $('#relevant-events').remove();
-    $('#no-relevant-events').removeClass("hide");
-}
+
+//guides user to create events or plans if none found
+if ($('.dropdown-items')[0].length < 1 ) {
+    $('.relevant-items').remove();
+    $('.no-relevant-items').removeClass('hide');
+};
+
