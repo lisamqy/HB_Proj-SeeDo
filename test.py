78addea57c1320057899ec2fc00fec6e6012c476
Lisa Ma <lisamqy@gmail.com>
2021-09-16 22:43:22 +0000

    Updated tests on test.py

--- test.py
+++ test.py
@@ -15,23 +15,35 @@ class ServerTests(unittest.TestCase):
     def test_homepage(self):
         result = self.client.get("/")
         if not session:
-            self.assertIn(b"New User?", result.data)
+            self.assertNotIn(b"Logout", result.data)
         elif session:
-            self.assertIn(b"View your account details", result.data)
+            self.assertIn(b"View account", result.data)
 
     def test_login_success(self):
         result = self.client.post("/",
                                     data={'Email': "user1@email.com", 'Password': "123"},
                                     follow_redirects=True)
-        self.assertNotIn(b"New User?", result.data)
-        self.assertIn(b"Welcome", result.data)
+        self.assertNotIn(b"Sign Up", result.data)
     
-    def test_registration_success(self):
-        result = self.client.post("/new",
-                                    data={'Username': "user99", 'Email': "user99@email.com", 'Password': "123"},
-                                    follow_redirects=True)
-        self.assertNotIn(b"Register Here!", result.data)
+    def test_registration_page(self):
+        result = self.client.get("/new")
+        self.assertIn(b"Register Here!", result.data)
+        self.assertIn(b"username", result.data)
 
+    def test_search_page(self):
+        result = self.client.get("/search")
+        self.assertIn(b"Event Search Results", result.data)
+
+    def test_user_page(self):
+        result = self.client.get("/user/10")
+        self.assertNotIn(b"Sign Up", result.data)
+        self.assertIn(b"Liked events", result.data)
+
+    def test_logout(self):
+        result = self.client.get("/goodbye",
+                                    follow_redirects=True)
+        self.assertNotIn(b"View Account", result.data)
+        self.assertIn(b"Sign Up", result.data)
 
 
 
